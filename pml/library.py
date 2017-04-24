b3edf84b33ac78b00da2863ae684359d8a7a0f0b
David Hadka <dmh309@arl.psu.edu>
2017-04-24 16:27:07 -0400

    Make script and constant lookups case insensitive

--- pml/library.py
+++ pml/library.py
@@ -6,9 +6,41 @@ import os
 from .exceptions import fail
 from .units import *
 
+# Case insensitive dictionary for string keys courtesy of http://stackoverflow.com/a/32888599
+class CaseInsensitiveDict(dict):
+    @classmethod
+    def _k(cls, key):
+        return key.lower() if isinstance(key, str) else key
+    def __init__(self, *args, **kwargs):
+        super(CaseInsensitiveDict, self).__init__(*args, **kwargs)
+        self._convert_keys()
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(self.__class__._k(key))
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(self.__class__._k(key), value)
+    def __delitem__(self, key):
+        return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key))
+    def __contains__(self, key):
+        return super(CaseInsensitiveDict, self).__contains__(self.__class__._k(key))
+    def has_key(self, key):
+        return super(CaseInsensitiveDict, self).has_key(self.__class__._k(key))
+    def pop(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).pop(self.__class__._k(key), *args, **kwargs)
+    def get(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).get(self.__class__._k(key), *args, **kwargs)
+    def setdefault(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).setdefault(self.__class__._k(key), *args, **kwargs)
+    def update(self, E={}, **F):
+        super(CaseInsensitiveDict, self).update(self.__class__(E))
+        super(CaseInsensitiveDict, self).update(self.__class__(**F))
+    def _convert_keys(self):
+        for k in list(self.keys()):
+            v = super(CaseInsensitiveDict, self).pop(k)
+            self.__setitem__(k, v)
+
 # These are currently stored internally, but could be changed later to a database
-CACHE = {}
-CONSTANTS = {}
+CACHE = CaseInsensitiveDict()
+CONSTANTS = CaseInsensitiveDict()
 
 def register_script(type, script, filename="script"):
     compiled_script = compile(script, filename, mode="exec")
@@ -24,7 +56,8 @@ def register_file(type, file):
         register_script(type, fp.read(), filename=file)
     
 # WARNING: This method modifies the current working directory so that filenames
-# can be given relative to the containing folder.    
+# can be given relative to the containing folder.  It will restore the original
+# directory when finished.
 def auto_register(folder):
     init_dir = os.getcwd()
     os.chdir(folder)
