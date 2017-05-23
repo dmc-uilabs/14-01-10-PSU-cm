45fdd5ea221154da71f93506f17c5ac553404947
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-05-23 16:00:22 -0500

    add response template back in

--- report.py
+++ report.py
@@ -359,10 +359,10 @@ if validate_graph(process_graph):
 
     outputs = "outputs="+str(inputs)
     outputs+= "\nfinalFileName="+final_name
-    # outputTemplate = "\noutputTemplate="+reportCSSString+reportTemplateString
+    outputTemplate = "\noutputTemplate=<h3>Report uploaded to: <a href=\"https://s3.amazonaws.com/psubucket01/"+final_name+"\">https://s3.amazonaws.com/psubucket01/"+final_name+"</a>"
 
     target = open("out.txt", 'w')
-    target.write(outputs)
+    target.write(outputs+outputTemplate)
     target.close
 
 else:
