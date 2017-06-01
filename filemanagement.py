c7325813785460ca6db50cf467fa229d12b59e99
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-06-01 12:43:48 -0500

    change to sec from ms

--- filemanagement.py
+++ filemanagement.py
@@ -44,7 +44,7 @@ def upload_report():
     k.set_contents_from_filename('./report.pdf')
 
     signed_url = conn.generate_url(
-           expires_in=1814400000,
+           expires_in=1814400,
            method='GET',
            bucket='psubucket01',
            key=k.key,
